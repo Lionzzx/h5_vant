03ccafaf2df22ce5345a40d053d1e9c896fc2e9b
womeifeng <caicai@qq.com>
2019-06-19 22:37:04 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
-const merge = require('webpack-merge')
-const autoprefixer = require('autoprefixer')
-const pxtorem = require('postcss-pxtorem')
-const tsImportPluginFactory = require('ts-import-plugin')
+const merge = require("webpack-merge");
+const autoprefixer = require("autoprefixer");
+const pxtorem = require("postcss-pxtorem");
+const tsImportPluginFactory = require("ts-import-plugin");
 module.exports = {
+  productionSourceMap: process.env.NODE_ENV === "production" ? false : true,
+  outputDir:
+    process.env.NODE_ENV === "development"
+      ? "dist/develop"
+      : "dist/shenzhen",
   css: {
     loaderOptions: {
       sass: {
@@ -10,10 +15,10 @@ module.exports = {
       },
       less: {
         modifyVars: {
-          red: '#fff',
-          blue: '#3eaf7c',
-          orange: '#f08d49',
-          'text-color': '#111',
+          red: "#fff",
+          blue: "#3eaf7c",
+          orange: "#f08d49",
+          "text-color": "#111",
         },
       },
       postcss: {
@@ -21,10 +26,10 @@ module.exports = {
           autoprefixer(),
           pxtorem({
             rootValue: 75,
-            propList: ['*',],
+            propList: ["*",],
             // 该项仅在使用 Circle 组件时需要
             // 原因参见 https://github.com/youzan/vant/issues/1948
-            selectorBlackList: ['van-circle__layer',],
+            selectorBlackList: ["van-circle__layer",],
           }),
         ],
       },
@@ -32,25 +37,25 @@ module.exports = {
   },
   chainWebpack: config => {
     config.module
-      .rule('ts')
-      .use('ts-loader')
+      .rule("ts")
+      .use("ts-loader")
       .tap(options => {
         options = merge(options, {
           transpileOnly: true,
           getCustomTransformers: () => ({
             before: [
               tsImportPluginFactory({
-                libraryName: 'vant',
-                libraryDirectory: 'es',
+                libraryName: "vant",
+                libraryDirectory: "es",
                 style: true,
               }),
             ],
           }),
           compilerOptions: {
-            module: 'es2015',
+            module: "es2015",
           },
-        })
-        return options
-      })
+        });
+        return options;
+      });
   },
-}
+};
