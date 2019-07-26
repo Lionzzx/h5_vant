6ce4f0242f9f269bd0794ae4caec1ee5a65ce251
Lionzzx <zexincaicai@163.com>
2019-07-26 17:46:37 +0800

    Fix

--- src/views/commercial/projectApplication/index.vue
+++ src/views/commercial/projectApplication/index.vue
@@ -2,48 +2,95 @@
   <div class="page">
     <div class="page-list">
       <div class="page-list-item x-fl">
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
+        <div class="title">高新技术企业认定</div>
+        <div>企业税收减免</div>
+        <div>享百万政府补贴</div>
         <div @click="navToMore" class="item-button">查看更多</div>
       </div>
       <div class="page-list-item x-fl">
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
+        <div class="title">高新餐品认定</div>
+        <div>助力产业转型</div>
+        <div>树立行业标杆</div>
         <div class="item-button">查看更多</div>
       </div>
       <div class="page-list-item x-fl">
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
+        <div class="title">创业带动就业</div>
+        <div>开业即可领取</div>
+        <div>政府直接补贴</div>
         <div class="item-button">查看更多</div>
       </div>
       <div class="page-list-item x-fl">
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
-        <div>国家高新技术企业认定</div>
+        <div class="title">创新基金</div>
+        <div>提升企业形象</div>
+        <div>加速通过红利申请</div>
+        <div class="item-button">查看更多</div>
+      </div>
+      <div class="page-list-item x-fl">
+        <div class="title">公司注册</div>
+        <div>足不出户 轻松办理</div>
+        <div>服务进度 实时掌控</div>
+        <div class="item-button">查看更多</div>
+      </div>
+      <div class="page-list-item x-fl">
+        <div class="title">公司变更</div>
+        <div>精简流程 简单快速</div>
+        <div>专人对接 高效详细</div>
+        <div class="item-button">查看更多</div>
+      </div>
+      <div class="page-list-item x-fl">
+        <div class="title">商标注册</div>
+        <div>当天报送材料</div>
+        <div>98%通过率</div>
+        <div class="item-button">查看更多</div>
+      </div>
+      <div class="page-list-item x-fl">
+        <div class="title">软著申请</div>
+        <div>国家荣誉认可</div>
+        <div>企业宣传利器</div>
         <div class="item-button">查看更多</div>
       </div>
     </div>
+    <div class="hasMore">加载更多</div>
+    <van-loading class="loading" type="spinner" color="#e94f55" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Loading } from 'vant';
 
 @Component({
-  components: {}
+  components: {
+    [Loading.name]: Loading
+  }
 })
 export default class Home extends Vue {
+  // private list = [{
+  //   title:'title'
+  // }];
   navToMore() {
-    this.$router.push({})
+    this.$router.push({});
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .page {
+  .hasMore{
+    font-size: 14px;
+    color: #e94f55;
+    line-height: 40px;
+    text-align: center;
+  }
+  .loading {
+    margin: 10px;
+    text-align: center;
+  }
+  .title {
+    font-size: 17px;
+    color: #e94f55;
+    margin-bottom: 10px;
+  }
   &-list {
     display: flex;
     justify-content: space-around;
@@ -62,9 +109,10 @@ export default class Home extends Vue {
       color: #e94f55;
       width: 80px;
       text-align: center;
-      line-height: 27px;
+      line-height: 25px;
+      margin-top: 20px;
       height: 27px;
-      border: 0.5px solid rgba(233, 79, 85, 1);
+      border: 1px solid rgba(233, 79, 85, 1);
       border-radius: 13px;
     }
   }
--- src/views/user/message/index.vue
+++ src/views/user/message/index.vue
@@ -1,57 +1,66 @@
 <template>
   <div>
-    <nav-bar title="我的消息" has-left></nav-bar>
-    <div v-for="(item, index) in list" :key="index" class="msg van-hairline--bottom">
-      <div class="msg-icon"><svg-icon :icon-class="item.icon" /></div>
-      <div class="msg-main">
-        <div class="msg-title">
-          <div class="msg-activity">{{ item.title }}</div>
-          <div class="msg-time">{{ item.time }}</div>
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <nav-bar title="我的消息" has-left></nav-bar>
+
+      <div v-for="(item, index) in list" :key="index" class="msg van-hairline--bottom">
+        <div class="msg-icon"><svg-icon icon-class="edit" /></div>
+        <!-- <div class="msg-main"> -->
+          <div class="msg-title">
+            <div class="msg-activity">{{ item.templatename }}</div>
+            <div class="msg-time">{{ item.senddate | changeTime }}</div>
+          <!-- </div> -->
+          <!-- <div class="msg-desc">{{ item.templatename }}</div> -->
         </div>
-        <div class="msg-desc">{{ item.desc }}</div>
       </div>
-    </div>
+    </van-list>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import NavBar from '@/components/NavBar/index.vue';
+import { List } from 'vant';
 
 @Component({
   components: {
-    NavBar
+    NavBar,
+    [List.name]: List
+  },
+  filters: {
+    changeTime: function(value: any) {
+      return value.substr(0, 11);
+    }
   }
 })
 export default class Message extends Vue {
-  private list = [
-    {
-      desc: '提示消息',
-      time: '2019-20',
-      title: '优惠活动',
-      icon: 'edit'
-    },
-    {
-      desc: '免费提醒',
-      time: '2019-20',
-      title: '优惠活动',
-      icon: 'table'
-    },
-    {
-      desc: '提示消息',
-      time: '2019-20',
-      title: '优惠活惠活惠活动',
-      icon: 'tree'
-    }
-  ];
+  private loading = false;
+  private finished = false;
+  private list: any = [];
+  private page = 0;
+  private pageSize = 20;
 
   created() {
     this.$storeApi.msgList({ page: 1, pageSize: 10 });
+    this.$storeApi.msgDetail({ id: 14644 });
+  }
+
+  async onLoad() {
+    this.page += 1;
+    const resp = await this.$storeApi.msgList({ page: this.page, pageSize: this.pageSize });
+    this.loading = false;
+    this.list.push(...resp.rows);
+    if (this.page * this.pageSize > resp.total) {
+      this.finished = true;
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.page {
+  min-height: calc(100vh - 2rem);
+}
 .msg {
   display: flex;
   padding: 10px 20px 10px 0;
@@ -63,6 +72,9 @@ export default class Message extends Vue {
 
   &-main {
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 
   &-title {
